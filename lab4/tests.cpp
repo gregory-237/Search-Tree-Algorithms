8536cae6769a91a8d7c4ec204b3b9ea2072d6084
Григорий Петров <petrovgrigorij29@gmail.com>
2024-04-14 20:14:05 +0400

    Fix stranger things

---
+++ lab4/tests.cpp
@@ -0,0 +1,78 @@
+#include "tree.h"
+
+int main() {
+	MySet tree;
+	tree.insert(40);
+	tree.insert(10);
+	tree.insert(57);
+	tree.insert(14);
+	tree.insert(45);
+	tree.insert(80);
+	tree.insert(9);
+	tree.insert(16);
+	tree.insert(47);
+	tree.insert(84);
+
+	tree.print();
+
+	tree.erase(50);
+	tree.print();
+
+	tree.erase(57);
+	tree.print();
+
+	cout << tree.contains(30);
+	cout << tree.contains(57);
+	cout << tree.contains(14);
+	cout << tree.contains(50);
+	cout << tree.contains(45);
+	cout << tree.contains(9);
+	cout << endl;
+
+	cout << "Tree:" << endl;
+	tree.print();
+	MySet tree2(tree);
+	cout << "Copy of Tree:" << endl;
+	tree2.print();
+
+	MySet tree3;
+	tree3.insert(8);
+	tree3.insert(2);
+	tree3.insert(9);
+	cout << "Third Tree:" << endl;
+	tree3.print();
+
+	tree3 = tree;
+
+	cout << "Third Tree after assignment:" << endl;
+	tree3.print();
+
+	MySet tree4;
+	tree4.insert(5);
+	tree4.insert(7);
+	tree4.insert(2);
+	tree4.insert(8);
+	tree4.insert(9);
+	tree4.insert(6);
+	tree4.insert(1);
+	cout << "Tree:" << endl;
+	tree4.print();
+
+	MySet tree5;
+	tree5.insert(6);
+	tree5.insert(5);
+	tree5.insert(9);
+	tree5.insert(2);
+	tree5.insert(3);
+	tree5.insert(4);
+	cout << "Second Tree:" << endl;
+	tree5.print();
+
+	cout << "Difference (tree - second tree):" << endl;
+	difference(tree4, tree5).print();
+	cout << "Difference (second tree - tree):" << endl;
+	difference(tree5, tree4).print();
+
+	cout << "Intersection:" << endl;
+	intersection(tree4, tree5).print();
+}
---
+++ lab4/tree.cpp
@@ -0,0 +1,169 @@
+﻿#include "tree.h"
+#include <random>
+#include <chrono>
+
+void average_filling(size_t size) {
+	double result_set = 0.0;
+	double result_vec = 0.0;
+	for (size_t i = 0; i < 100; i++)
+	{
+		mt19937 gen(i);
+		uniform_int_distribution<> distrib(-2000000, 2000000);
+		MySet set;
+		vector<int> vec;
+		auto start_set = chrono::high_resolution_clock::now();
+		for (size_t i = 0; i < size; i++)
+		{
+			set.insert(distrib(gen));
+		}
+		auto end_set = chrono::high_resolution_clock::now();
+		auto start_vec = chrono::high_resolution_clock::now();
+		for (size_t i = 0; i < size; i++)
+		{
+			vec.push_back(distrib(gen));
+		}
+		auto end_vec = chrono::high_resolution_clock::now();
+		double seconds_set = chrono::duration<double>(end_set - start_set).count();
+		double seconds_vec = chrono::duration<double>(end_vec - start_vec).count();
+		result_set += seconds_set / 100;
+		result_vec += seconds_vec / 100;
+	}
+	cout << "SET Average(100) filling time with " << size << " items: " << result_set << endl;
+	cout << "VEC Average(100) filling time with " << size << " items: " << result_vec << endl;
+}
+
+void average_finding(size_t size) {
+	double result_set = 0.0;
+	double result_vec = 0.0;
+	MySet set;
+	vector<int> vec;
+	mt19937 gen(30);
+	uniform_int_distribution<> distrib(-2000000, 2000000);
+	for (size_t i = 0; i < size; i++)
+	{
+		auto t = distrib(gen);
+		set.insert(t);
+		vec.push_back(t);
+	}
+	for (size_t i = 0; i < 1000; i++)
+	{
+		auto t = distrib(gen);
+		auto start_set = chrono::high_resolution_clock::now();
+		set.contains(t);
+		auto end_set = chrono::high_resolution_clock::now();
+		auto start_vec = chrono::high_resolution_clock::now();
+		find(vec.begin(), vec.end(), t);
+		auto end_vec = chrono::high_resolution_clock::now();
+		double seconds_set = chrono::duration<double>(end_set - start_set).count();
+		double seconds_vec = chrono::duration<double>(end_vec - start_vec).count();
+		result_set += seconds_set / 1000;
+		result_vec += seconds_vec / 1000;
+	}
+	cout << "SET Average(1000) finding time in " << size << " items: " << result_set << endl;
+	cout << "VEC Average(1000) finding time in " << size << " items: " << result_vec << endl;
+}
+
+void average_erasing(size_t size) {
+	double result_set = 0.0;
+	double result_vec = 0.0;
+	MySet set;
+	vector<int> vec;
+	mt19937 gen(30);
+	uniform_int_distribution<> distrib(-2000000, 2000000);
+	for (size_t i = 0; i < size; i++)
+	{
+		auto t = distrib(gen);
+		set.insert(t);
+		vec.push_back(t);
+	}
+	for (size_t i = 0; i < 1000; i++)
+	{
+		bool del_set = false;
+		bool del_vec = false;
+		int deleted = distrib(gen);
+		auto start_set = chrono::high_resolution_clock::now();
+		if (set.erase(deleted)) {
+			del_set = true;
+		}
+		auto end_set = chrono::high_resolution_clock::now();
+		auto start_vec = chrono::high_resolution_clock::now();
+		for (size_t i = 0; i < size; i++)
+		{
+			if (vec[i] == deleted) {
+				vec.erase(vec.begin() + i);
+				del_vec = true;
+				break;
+			}
+		}
+		auto end_vec = chrono::high_resolution_clock::now();
+		if (del_set) {
+			set.insert(deleted);
+		}
+		if (del_vec) {
+			vec.push_back(deleted);
+		}
+		double seconds_set = chrono::duration<double>(end_set - start_set).count();
+		double seconds_vec = chrono::duration<double>(end_vec - start_vec).count();
+		result_set += seconds_set / 1000;
+		result_vec += seconds_vec / 1000;
+	}
+	cout << "SET Average(1000) erasing time in " << size << " items: " << result_set << endl;
+	cout << "VEC Average(1000) erasing time in " << size << " items: " << result_vec << endl;
+}
+
+void average_insertion(size_t size) {
+	double result_set = 0.0;
+	double result_vec = 0.0;
+	MySet set;
+	vector<int> vec;
+	mt19937 gen(30);
+	uniform_int_distribution<> distrib(-2000000, 2000000);
+	for (size_t i = 0; i < size; i++)
+	{
+		auto t = distrib(gen);
+		set.insert(t);
+		vec.push_back(t);
+	}
+	for (size_t i = 0; i < 1000; i++)
+	{
+		bool ins_set = false;
+		bool ins_vec = false;
+		int inserted = distrib(gen);
+		auto start_set = chrono::high_resolution_clock::now();
+		if (set.insert(inserted)) {
+			ins_set = true;
+		}
+		auto end_set = chrono::high_resolution_clock::now();
+		auto start_vec = chrono::high_resolution_clock::now();
+		vec.push_back(inserted);
+		auto end_vec = chrono::high_resolution_clock::now();
+		if (ins_set) {
+			set.erase(inserted);
+		}
+		vec.pop_back();
+		double seconds_set = chrono::duration<double>(end_set - start_set).count();
+		double seconds_vec = chrono::duration<double>(end_vec - start_vec).count();
+		result_set += seconds_set / 1000;
+		result_vec += seconds_vec / 1000;
+	}
+	cout << "SET Average(1000) insertion time in " << size << " items: " << result_set << endl;
+	cout << "VEC Average(1000) insertion time in " << size << " items: " << result_vec << endl;
+}
+
+int main() {
+	average_filling(1000);
+	average_filling(10000);
+	average_filling(100000);
+
+	average_finding(1000);
+	average_finding(10000);
+	average_finding(100000);
+
+	average_erasing(1000);
+	average_erasing(10000);
+	average_erasing(100000);
+
+	average_insertion(1000);
+	average_insertion(10000);
+	average_insertion(100000);
+}
